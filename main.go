f06fec6b235f705967ceb571016b2c73ca886f73
wanzulfikri <wanzulfikri@gmail.com>
2020-03-22 01:13:46 +0800

    Add error returning and error handling codes to various sections in main.go

--- main.go
+++ main.go
@@ -17,15 +17,44 @@ var workType = map[bool]string{
 	false: "Rest",
 }
 
+// Set the sounds for the timer here.
+//
+// Three types of sounds:
+//	i)  work  - after every odd interval
+//  ii) rest  - after every even interval
+//  ii) end   - at the end of program
+//
+//  Example: if program input is 10, 5, 10, then the sequence of sound would be
+//	work -> rest -> work -> end.
+var sounds = map[string]string{
+	// modify according to your file name
+	"work": "ohyeah.wav",
+	"rest": "ding.wav",
+	"end":  "cheers.wav",
+}
+
+// timeModifier will be multiplied by program input to give actual timer duration.
+// In testing, modify this to time.Millisecond or time.Nanosecond to shorten
+// test duration.
+var timeModifier = time.Minute
+
 func main() {
 	intervals := os.Args[1:]
-	len := len(intervals)
-	workBuffer, format := getBuffer("ohyeah.wav")
-	restBuffer, _ := getBuffer("ding.wav")
+	if len(intervals) == 0 {
+		log.Fatal("No timer interval added. Please rerun the timer with at least one interval")
+	}
+
+	err := checkValidity(intervals)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	workBuffer, format := getBuffer(sounds["work"])
+	restBuffer, _ := getBuffer(sounds["rest"])
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 	isWorking := true
 	buffer := workBuffer
-	for i := 0; i < len; i++ {
+	for i := 0; i < len(intervals); i++ {
 		if isWorking {
 			buffer = workBuffer
 		} else {
@@ -35,15 +64,27 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		timer := time.NewTimer(time.Duration(minute) * time.Minute)
+		timer := time.NewTimer(time.Duration(minute) * timeModifier)
 		sound := buffer.Streamer(0, buffer.Len())
-		fmt.Printf("%v %v %v ", i, workType[isWorking], time.Now().Format(time.Kitchen))
+		fmt.Printf("%v %v: %v -> %v", i, workType[isWorking], time.Now().Format(time.Kitchen), time.Now().Add(timeModifier*time.Duration(minute)).Format(time.Kitchen))
 		<-timer.C
 		speaker.Play(sound)
 		isWorking = !isWorking
 	}
 	time.Sleep(time.Second * 2)
-	playOnce("cheers.wav")
+	playOnce(sounds["end"])
+}
+
+func checkValidity(intervals []string) error {
+	for _, interval := range intervals {
+		minute, err := strconv.Atoi(interval)
+		if err != nil {
+			return fmt.Errorf("You entered %q. The interval must be an integer", interval)
+		} else if minute <= 0 {
+			return fmt.Errorf("You entered %q. The interval must be more than 0", interval)
+		}
+	}
+	return nil
 }
 
 func playOnce(filename string) {
